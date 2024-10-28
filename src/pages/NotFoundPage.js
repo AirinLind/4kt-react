da732b3a15b73387a6340a5408137aeaa44406c4
AirinLind <113242888+AirinLind@users.noreply.github.com>
2024-10-28 19:39:22 +0300

    28.10

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -18,7 +18,7 @@ function NotFoundPage(){
     return(
         <div className="not_found">
             <button className="btn" onClick={() => navigate('/products')}>Go to Prod List</button>
-            <img src="https://img.freepik.com/free-vector/404-error-template-flat-style_23-2147757687.jpg?t=st=1730133312~exp=1730136912~hmac=861fd0184a5df98bbc9c53e4df464d69a19a20ce87fcde03362c38b39082377e&w=740"/>
+            <img src="https://img.freepik.com/free-vector/error-404-concept-illustration_114360-1811.jpg?t=st=1730133489~exp=1730137089~hmac=282680c7ce1dd0b0faaf2e761ba216368b2d4b9657b67bcf1037a2477af8305d&w=900"/>
         </div>
     )
 }
