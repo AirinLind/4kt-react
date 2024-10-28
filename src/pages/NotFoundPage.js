a9c83414687c21503a798abf61314bf17b0a9aa9
AirinLind <113242888+AirinLind@users.noreply.github.com>
2024-10-28 19:37:44 +0300

    Update NotFoundPage.js

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -18,7 +18,7 @@ function NotFoundPage(){
     return(
         <div className="not_found">
             <button className="btn" onClick={() => navigate('/products')}>Go to Prod List</button>
-            <img src="https://img.freepik.com/free-vector/page-found-concept-illustration_114360-1869.jpg"/>
+            <img src="https://img.freepik.com/free-vector/404-error-template-flat-style_23-2147757687.jpg?t=st=1730133312~exp=1730136912~hmac=861fd0184a5df98bbc9c53e4df464d69a19a20ce87fcde03362c38b39082377e&w=740"/>
         </div>
     )
 }
